9d4daf7f932f0dd24b8e18b78f40e13e4ffa291a
ippo579 <lengareshreyash@gmail.com>
2023-10-09 16:13:19 +0530

    10 commit

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo /home/ec2-user/apache-tomcat-9.0.73/bin/./server_start.sh
+sudo /home/ec2-user/apache-tomcat-9.0.73/bin/./startup.sh
--- scripts/server_stop.sh
+++ scripts/server_stop.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo /home/ec2-user/apache-tomcat-9.0.73/bin/./server_stop.sh
+sudo /home/ec2-user/apache-tomcat-9.0.73/bin/./shutdown.sh
