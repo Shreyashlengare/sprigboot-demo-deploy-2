35d3583128d060b78b8448214b215701ab31e7c8
ippo579 <lengareshreyash@gmail.com>
2023-10-27 10:43:43 +0530

    6 commit

--- scripts/server_stop.sh
+++ scripts/server_stop.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 sudo /home/ubuntu/apache-tomcat-10.1.14/bin/shutdown.sh
+
+# Give some time for Tomcat to gracefully shut down
+sleep 5
+
+# Find and kill the Tomcat process (adjust the command as needed)
+sudo pkill -f "catalina"
