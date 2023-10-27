3da8c205a383edf2c25310d2e5d9f881cfd13691
ippo579 <lengareshreyash@gmail.com>
2023-10-27 13:17:04 +0530

    10 commit

--- scripts/server_stop.sh
+++ scripts/server_stop.sh
@@ -8,7 +8,7 @@ LISTENING_PROCESS=$(lsof -i :8082 | awk 'NR==2{print $2}')
 
 if [ -n "$LISTENING_PROCESS" ]; then
   echo "A process is listening on port 8082 (PID: $LISTENING_PROCESS). Stopping it..."
-  kill $LISTENING_PROCESS
+  sudo kill -9 $LISTENING_PROCESS
 else
   echo "No process found listening on port 8082."
 fi
--- src/main/java/com/backend/controller/EmployeeController.java
+++ src/main/java/com/backend/controller/EmployeeController.java
@@ -106,7 +106,7 @@ public class EmployeeController {
     @GetMapping({"/demo"})
     public String emp() {
         return
-                "Hi, ippopay!";
+                "Vannakkam, ippopay!";
     }
 
 }
