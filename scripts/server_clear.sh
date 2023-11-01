abf8c2e5c7c98f3ab65f186c82f56c7ed877cf01
ippo579 <lengareshreyash@gmail.com>
2023-11-01 12:26:44 +0530

    4 commit

--- scripts/server_clear.sh
+++ scripts/server_clear.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 rm -rf /home/ec2-user/server
-chmod 750 /home/ec2-user/apache-tomcat-10.1.14/webapps/
+sudo chown -R ubuntu:ubuntu /home/ec2-user/apache-tomcat-10.1.14/webapps/
+sudo chmod -R 750 /home/ec2-user/apache-tomcat-10.1.14/webapps/
