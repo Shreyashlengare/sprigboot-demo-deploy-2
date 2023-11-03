7bcbe8c1f392f6f612e83df5b109ac4fc4e7eab7
ippo579 <lengareshreyash@gmail.com>
2023-11-03 10:15:37 +0530

    1 commit

--- scripts/server_clear.sh
+++ scripts/server_clear.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 rm -rf /home/ec2-user/server
-sudo chown -R ubuntu:ubuntu /home/ec2-user/apache-tomcat-10.1.14/webapps/
-sudo chmod -R 750 /home/ec2-user/apache-tomcat-10.1.14/webapps/
 sudo systemctl daemon-reload
 sudo systemctl restart BackendApp
 sudo systemctl enable BackendApp
