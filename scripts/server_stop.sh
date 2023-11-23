189991006a151a61b734c4d8d644abdcd11b2dc3
ippo579 <lengareshreyash@gmail.com>
2023-11-23 14:13:50 +0530

    2 change

--- scripts/server_stop.sh
+++ scripts/server_stop.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Define the port number you want to free
-PORT_TO_FREE=8082
+PORT_TO_FREE=8081
 
 # Check if a process is listening on the specified port
-LISTENING_PROCESS=$(lsof -i :8082 | awk 'NR==2{print $2}')
+LISTENING_PROCESS=$(lsof -i :8081 | awk 'NR==2{print $2}')
 
 if [ -n "$LISTENING_PROCESS" ]; then
-  echo "A process is listening on port 8082 (PID: $LISTENING_PROCESS). Stopping it..."
+  echo "A process is listening on port 8081 (PID: $LISTENING_PROCESS). Stopping it..."
   sudo kill -9 $LISTENING_PROCESS
   echo "(PID: $LISTENING_PROCESS). Stopped it..."
 else
-  echo "No process found listening on port 8082."
+  echo "No process found listening on port 8081."
 fi
