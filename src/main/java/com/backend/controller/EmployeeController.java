f19d2af3bc5cb4550510bfce674f3d1088b2894c
ippo579 <lengareshreyash@gmail.com>
2023-11-23 15:12:54 +0530

    2 change

--- src/main/java/com/backend/controller/EmployeeController.java
+++ src/main/java/com/backend/controller/EmployeeController.java
@@ -106,7 +106,7 @@ public class EmployeeController {
     @GetMapping({"/demo"})
     public String emp() {
         return
-                "Vannakkam, ippopay!";
+                "Hello, ippopay!";
     }
 
 }
