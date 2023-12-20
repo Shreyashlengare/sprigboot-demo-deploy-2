f4b40d133c84d856db0363e5914293cfc24dbfc5
ippo579 <lengareshreyash@gmail.com>
2023-12-20 13:15:26 +0530

    2 change for the day for jenkins

--- src/main/java/com/backend/controller/EmployeeController.java
+++ src/main/java/com/backend/controller/EmployeeController.java
@@ -106,7 +106,7 @@ public class EmployeeController {
     @GetMapping({"/demo"})
     public String emp() {
         return
-                "Namaste, ippopay!";
+                "hello there, ippopay!";
     }
 
 }
